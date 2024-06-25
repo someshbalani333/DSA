4e10dd5099557710c1820d76d4b8852cd30e1e3f
someshbalani333 <69295309+someshbalani333@users.noreply.github.com>
2024-06-25 19:53:37 +0530

    Create 4_trailing_zeroes_in_factorial.js

---
+++ Mathematics/4_trailing_zeroes_in_factorial.js
@@ -0,0 +1,31 @@
+// Brute force
+const trailingZeroesBrute = (n) => {
+    let fac = 1;
+    while(n > 0) {
+        fac = fac*n;
+        n--;
+    }
+
+    let res = 0;
+    while(fac % 10 === 0) {
+        res++;
+        fac = Math.floor(fac/10);
+    }
+
+    return res;
+}
+
+trailingZeroesBrute(10)
+
+const trailingZeroesOptimized = (n) => {
+    let res = 0;
+    for(let i = 5; i <= n; i = i * 5) {
+        res = res + Math.floor(n/i);
+    }
+
+    console.log(res);
+
+    return res;
+}
+
+trailingZeroesOptimized(251);
